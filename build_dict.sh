b5a9a9e9199d84c4ca3cc3243c2e9d55da8ebc93
lovac42 <lovac42@protonmail.com>
2020-04-07 04:46:45 -0400

    rm unused files

--- build_dict.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-KANJIDIC=common/data/kanjidic
-EDICT=common/data/edict
-ENAMDICT=common/data/enamdict
-DB=yomi_base/japanese/dictionary.db
-
-common/compile.py --kanjidic $KANJIDIC --edict $EDICT $DICT --enamdict $ENAMDICT --db $DB
--- build_zip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ZIP=yomichan.zip
-rm -f $ZIP
-7z a yomichan.zip -xr\!\*.pyc yomichan.py yomi_base
