65eeeaa86a696f280d5f7ff5967e345244dc49ad
lovac42 <lovac42@protonmail.com>
2020-04-07 18:57:28 -0400

    Added cp and info. Changed titles in ui and rm update cb

---
+++ ui/header.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'about.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui, QtGui as QtWidgets
---
+++ ui/make.bat
@@ -0,0 +1,23 @@
+call :build_ui about
+call :build_ui preferences
+call :build_ui reader
+call :build_ui updates
+pause
+exit
+
+
+:build_ui
+echo building %1.ui...
+
+pyuic5 %1.ui -o out.py
+
+type header.py >%1_ui.py
+
+findstr /V "# PyQt5 resources_rc" out.py>>%1_ui.py
+
+if /i not %1==updates echo from . import resources_rc>>%1_ui.py
+
+del out.py
+move %1_ui.py ..\src\yomi_obaasan\gen
+
+exit /b
